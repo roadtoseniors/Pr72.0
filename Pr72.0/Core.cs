a0240bb68590657cd70dd74b39493f139f8ec229
234846 <234846@KB37-121-C08>
2025-10-28 10:48:47 +0300

    Декомпазиция

---
+++ Pr72.0/Core.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pr72._0
+{
+    internal class Core
+    {
+
+        public static vovamp4Entities1 Context = new vovamp4Entities1();
+    }
+}
--- Pr72.0/Program.cs
+++ Pr72.0/Program.cs
@@ -6,10 +6,69 @@ using System.Threading.Tasks;
 
 namespace Pr72._0
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
         }
     }
+
+    class Part
+    {
+    }
+
+    class CoreFile
+    {
+        public static object Context { get; set; }
+    }
+
+    class Client
+    {
+        public void GenerateBrokenPart()
+        {
+        }
+
+        public int GetRepairCost()
+        {
+        }
+    }
+
+    class GameManager
+    {
+        public void InitializeGame()
+        {
+        }
+
+        public void ProcessClient()
+        {
+        }
+
+        public void UpdateBalance()
+        {
+        }
+
+        public void CheckGameOver()
+        {
+        }
+    }
+
+    class Warehouse
+    {
+        public Part GetPart()
+        {
+        }
+
+        public void AddPart()
+        {
+        }
+
+        public void RemovePart()
+        {
+        }
+
+        public bool CheckAvailability()
+        {
+        }
+    }
+    
 }
